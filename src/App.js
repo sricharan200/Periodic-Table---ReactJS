09909dfaeb2d8c97fdd6ac4b3677a13f8673e8c7
sricharan200 <148656478+sricharan200@users.noreply.github.com>
2024-05-22 22:44:44 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import PeriodicTable from "./PeriodicTable";
+// import Video from "./Video";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Periodic Table of Elements</h1>
+      <PeriodicTable />
+      {/* <Video /> */}
+    </div>
+  );
+}
+
+export default App;
